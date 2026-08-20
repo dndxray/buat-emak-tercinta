d142ac884c1f5e5cab73f33918935ccb56cf3507
Dinda Isyariani <isyarianid2@g,ail.com>
2026-08-20 23:01:16 +0700

    update: bikin layout A4 dan badge lingaran merah

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function downloadPDF() {
-    alert('Download PDF');
+    // Ambil container buat pdf
+    const element = document.getElementById('print-container');
+    
+    // ketentuan pdf
+    const opt = {
+        margin: 4,
+        filename: 'download1.pdf',
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+    };
+    // generate pdf
+    html2pdf().set(opt).from(element).save();
 }
